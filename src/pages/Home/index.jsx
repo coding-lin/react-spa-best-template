8c8ac23ee40ebc0c576c2e532212b1e71a81a13d
hjl <2764881569@qq.com>
2022-06-16 15:35:30 +0800

    react-spa-best-template

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import React, { useState, UseEffect } from "react";
+// import {} from './style'
+
+const Home = () => {
+  return (
+    <div>
+      Home
+    </div>
+  )
+}
+
+export default Home
